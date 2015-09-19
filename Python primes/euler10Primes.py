43b3252b58f56ed30dffc93452d544b197387904
Russell Tan <sell_nat@yahoo.com>
2015-09-19 15:44:50 -0700

    Starting python primes assignment

---
+++ Python primes/euler10Primes.py
@@ -0,0 +1,29 @@
+import math
+
+def is_prime( num ):
+	if num < 2:
+		return False
+	
+	if num == 2:
+		return True
+	
+	for value in range(3,math.ceil(math.sqrt(num))):
+		if num % value == 0 :
+			return False
+			
+	return True
+	
+def get_primes( num ):
+	primes = []
+	
+	for value in range(0,num):
+		if is_prime(value):
+			primes.append(value)
+			
+	return primes
+	
+def sum_primes():
+	return 0
+    
+#main
+print(get_primes(100))
