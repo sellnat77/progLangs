44b20f2eb9ad50037e54f0716a87be4874b02f01
Russell Tan <sell_nat@yahoo.com>
2015-09-20 20:15:37 -0700

    Added documentation

--- Python primes/euler10Primes.py
+++ Python primes/euler10Primes.py
@@ -1,6 +1,10 @@
 import math
 
+#Target value to find sum
+target = 2e6
+
 def is_prime( num ):
+	"Returns whether or not a value is prime"
 	if num < 2:
 		return False
 	
@@ -9,7 +13,8 @@ def is_prime( num ):
 		
 	if num % 2 == 0:
 		return False
-	
+	#If num can be divided by any number between 3- ciel(sqrt(num+1))
+	#then it is not a prime number
 	for value in range(3,math.ceil(math.sqrt(num+1))):
 		if num % value == 0 :
 			return False
@@ -17,22 +22,24 @@ def is_prime( num ):
 	return True
 	
 def get_primes( num ):
-	for value in range(num,999999999):
+	"Yields all prime numbers from num-target"
+	for value in range(num,int(target)):
 		if is_prime(value):
 			yield value
 	
 def sum_primes():
-
+	"Adds all the results from get_primes starting from 0"
 	sum = 0
 
 	for val in get_primes(0):
-		#print (val)
-		if (val > 2e6):
+		if (val > target):
 			break
 		sum += val		
 		
 	return sum
 		
-    
 #main
-print(sum_primes())
+print("The sum is: ",sum_primes())
+
+
+
