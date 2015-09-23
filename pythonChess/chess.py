65d0efca1332453e74d13fa567d2b27c807e5561
Russell Tan <sell_nat@yahoo.com>
2015-09-23 14:40:14 -0700

    Working on pawns

--- pythonChess/chess.py
+++ pythonChess/chess.py
@@ -1,6 +1,6 @@
 boardSize = 8
 boardRows = [1,2,3,4,5,6,7,8]
-boardCols = ['A','B','C','D','E','F','G','H']
+boardCols = [1,2,3,4,5,6,7,8]
 currBoard = [[None for _ in range(8)] for _ in range(8)]
 playerOne = 'P1'
 playerTwo = 'P2'
@@ -32,22 +32,45 @@ def print_board():
 			count += 1
 
 def moves_king(board,player,tup):
-	row, col = tup
+	col, row = tup
+	
+	
 	
 def moves_queen(board,player,tup):
-	row, col = tup
+	col, row = tup
 	
-def moves_knight(board,player,tup):
-	row, col = tup
-	
+def moves_knight(board,player,tup):	
+	col, row = tup
+
 def moves_pawn(board,player,tup):
-	row, col = tup
+	allMoves = []
+	col, row = tup
+	
+	#if same piece
+		#if not playerOne
+	#if enemy diagonal
+	#if enemy in front
+		#if (col, row+1).unit is not playerTwo
+	#fwd = (col,row+1),(col,row+1)
+		#Check if not past the board
+		#if col+1 or col -1 is not > 8 or < 0
+	#diag = (col+1,row+1),(col-1,row+1)
+	
+	if player == playerOne:
+		
+		allMoves.append((col,row+1))
+		allMoves.append((col,row+2))
+	else:
+		allMoves.append((col,row-1))
+		allMoves.append((col,row-2))
+		
+	return allMoves
 	
 def moves_rook(board,player,tup):
-	row, col = tup
+	col, row = tup
 	
 def moves_bishop(board,player,tup):
-	row, col = tup
+	col, row = tup
 	
 def moves_function(unit):
 	return{
@@ -60,15 +83,34 @@ def moves_function(unit):
 	}.get(unit,moves_pawn)
 
 def player_pieces(board,player):
-	#generator
+	#yields type of piece, pieces coordinates
+	for rw in board:
+		for point in rw:
+			unit, col, row, locPlayer = point
+			if player == locPlayer:
+				yield((unit,(col,row)))
 	
 def possible_moves(board, player):
-	#use player pieces
+	count = 1
+	for unit in player_pieces(board,player):
+		piece,location = unit
+		moves = moves_function(piece)
+		
+		print("Moves for player",player,"'s ",piece," at ",location)
+		for mov in moves(board,player,location):
+			if count % 2 == 0:
+				print("")
+			print("\tHeres a move!",count, end="")
+			print(mov)
+			count += 1
+		print("")
 	
-
-
-
-
+	#use player pieces
 			
 init_board()
 print_board()
+
+print("Requesting player 2")
+possible_moves(currBoard,playerTwo)
+print("Requesting player 1")
+possible_moves(currBoard,playerOne)
