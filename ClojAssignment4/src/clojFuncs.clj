9d25a395065482e73d107010141fcf9318cf6369
sellnat77 <sell_nat@yahoo.com>
2015-10-19 21:29:20 -0700

    Working on hw assignment

--- ClojAssignment4/src/clojFuncs.clj
+++ ClojAssignment4/src/clojFuncs.clj
@@ -1,1 +1,11 @@
+(defn mylast [coll]
+    (if ( = 0 (count coll))
+      nil)
+    (if (= 1 (count coll))
+      (first coll))
+    (myLast (next coll)))
 
+(defn mySum [coll] temp
+  (if ( = 0 (count coll)
+        temp)
+    ( + temp (mySum (next coll)))))
