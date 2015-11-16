164338dd89db9109bff27c3d01e095ae66697cf8
sellnat77 <sell_nat@yahoo.com>
2015-11-15 19:41:52 -0800

    Tried moving all functionality within switch context

--- ObjectsInC/OOc/OOc/objects.c
+++ ObjectsInC/OOc/OOc/objects.c
@@ -1,36 +1,160 @@
 #include<stdio.h>
 
+typedef enum { ANIMAL, DOG, CAT, LION } selectedAnimal;
 
-
+//DECLARE STRUCTS
 struct Animal
 {
-	void **v_table;
+	void (**v_table)();
 	int age;
 };
 
 struct Dog
 {
-	void **v_table;
+	void (**v_table)();
 	int age;
 	double weight;
 };
 
 struct Cat
 {
-	void **v_table;
+	void (**v_table)();
 	int age;
 	int numberOfLives;
 };
 
 struct Lion
 {
-	void **v_table;
+	void (**v_table)();
 	int age;
 	int numberOfLives;
 	double weight;
 };
 
-typedef enum { ANIMAL, DOG, CAT, LION } selectedAnimal;
+//SPEAK FUNCTIONS
+void Speak_Dog(struct Animal *a);
+void Speak_Cat(struct Animal *a);
+void Speak_Lion(struct Animal *a);
+
+//COST FUNCTIONS
+double Get_Cost_Dog(struct Animal *a);
+double Get_Cost_Cat(struct Animal *a);
+
+//VTABLE ASSIGNMENTS
+void (*v_table_dog[])();
+void (*v_table_cat[])();
+void (*v_table_lion[])();
+
+//CONSTRUCTORS
+void Construct_Dog(struct Animal *a);
+void Construct_Cat(struct Animal *a);
+void Construct_Lion(struct Animal *a);
+
+//OPERATION FUNCTIONS
+void animal_Operations(int selection);
+
+int main()
+{
+	struct Animal *myAnimal = 1;
+	int animalSelection;
+	int age;
+	int numLives;
+	double weight;
+	double cost;
+
+	printf("Please choose an animal:\n");
+	printf("1.)Dog\n");
+	printf("2.)Cat\n");
+	printf("3.)Lion\n");
+	scanf_s("%d", &animalSelection);
+
+	switch (animalSelection)
+	{
+	case ANIMAL:
+		printf("Selected base animal");
+		break;
+
+	case DOG:
+		(struct Animal*) myAnimal = (struct Animal*)malloc(sizeof(struct Dog));
+		
+		if (myAnimal != NULL)
+		{
+			printf("Selected dog\n");
+			printf("Enter dog's weight: ");
+			scanf_s("%lf", &weight);
+			printf("Enter dog's age:");
+			scanf_s("%d", &age);
+
+			Construct_Dog(&myAnimal);
+			((struct Dog*)myAnimal)->weight = weight;
+			((struct Dog*)myAnimal)->age = age;
+
+			printf("\n");
+			printf("\nSpeak animal!");
+			((void(*)(struct Animal*))&myAnimal->v_table[0])(myAnimal);
+			cost = ((double(*)(struct Animal*))&myAnimal->v_table[1])(myAnimal);
+			//(myAnimal->v_table[1])(myAnimal);
+			printf("\nYour animal costs %.2lf per month.\n", cost);
+
+			free(&myAnimal);
+		}
+		else
+		{
+			break;
+		}
+		break;
+
+	case CAT:
+		myAnimal = malloc(sizeof(struct Cat));
+		
+		printf("Selected cat");
+		printf("Enter cat's number of lives:");
+		scanf_s("%d", &numLives);
+		printf("Enter cat's age: %d");
+		scanf_s("%d", &age);
+
+		Construct_Cat(&myAnimal);
+		((struct Cat*)myAnimal)->numberOfLives = numLives;
+		((struct Cat*)myAnimal)->age = age;
+
+		printf("\n");
+		printf("\nSpeak animal!");
+		((void(*)(struct Animal*))&myAnimal->v_table[0])(myAnimal);
+		cost = ((double(*)(struct Animal*))&myAnimal->v_table[1])(myAnimal);
+		//(myAnimal->v_table[1])(myAnimal);
+		printf("\nYour animal costs %.2lf per month.\n", cost);
+
+		free(&myAnimal);
+		break;
+
+	case LION:
+		myAnimal = malloc(sizeof(struct Lion));
+		printf("Selected lion");
+		scanf_s("Enter lion's weight: %lf", &weight);
+		scanf_s("Enter lion's number of lives: %d", &numLives);
+		scanf_s("Enter lion's age: %d", &age);
+		Construct_Lion(&myAnimal);
+		((struct Lion*)myAnimal)->weight = weight;
+		((struct Lion*)myAnimal)->numberOfLives = numLives;
+		((struct Lion*)myAnimal)->age = age;
+
+		printf("\n");
+		printf("\nSpeak animal!");
+		((void(*)(struct Animal*))&myAnimal->v_table[0])(myAnimal);
+		cost = ((double(*)(struct Animal*))&myAnimal->v_table[1])(myAnimal);
+		//(myAnimal->v_table[1])(myAnimal);
+		printf("\nYour animal costs %.2lf per month.\n", cost);
+
+		free(&myAnimal);
+		break; 
+
+	default:
+		break;
+	}
+
+	
+	return 1;
+}
 
 void Speak_Dog(struct Animal *a)
 {
@@ -49,28 +173,23 @@ void Speak_Lion(struct Animal *a)
 
 double Get_Cost_Dog(struct Animal *a)
 {
-	double cost = (((struct Dog*)a)->weight / 3) * 7.50;
-	
-	return cost;
+	return (((struct Dog*)a)->weight / 3) * 7.50;
 };
 
 double Get_Cost_Cat(struct Animal *a)
 {
-	double cost = (((struct Cat*)a)->age * 5.50) + 100.00;
-
-	return cost;
+	return (((struct Cat*)a)->age * 5.50) + 100.00;
 };
 
-void* v_table_dog[2] = { Speak_Dog, Get_Cost_Dog };
-void* v_table_cat[2] = { Speak_Cat, Get_Cost_Cat };
-void* v_table_lion[2] = { Speak_Lion, Get_Cost_Cat };
+void (*v_table_dog[])()  = { &Speak_Dog , &Get_Cost_Dog };
+void (*v_table_cat[])() = { &Speak_Cat , &Get_Cost_Cat };
+void (*v_table_lion[])() = { &Speak_Lion, &Get_Cost_Cat };
 
 void Construct_Dog(struct Animal *a)
 {
 	((struct Dog*)a)->v_table = v_table_dog;
 	((struct Dog*)a)->age = 0;
 	((struct Dog*)a)->weight = 0;
-
 };
 
 void Construct_Cat(struct Animal *a)
@@ -88,85 +207,22 @@ void Construct_Lion(struct Animal *a)
 	((struct Lion*)a)->weight = 0;
 }
 
-int main()
+void animal_Operations(int selection)
 {
-	struct Dog *dogPtr;
-	struct Cat *catPtr;
-	struct Lion *lionPtr;
-	struct Animal *myAnimal = 1;
-	int animalSelection;
-	int age;
-	int numLives;
 	double weight;
+	int age;
+	struct Animal* myAnimal = (struct Animal*)malloc(sizeof(struct Dog));
 
-	
-	//printf("Printing the dog:\n\n\n\n");
-	//((void (*)(struct Animal*))myAnimal->v_table[0])(myAnimal);
-
-	
-	printf("Please choose an animal:\n");
-	printf("1.)Dog\n");
-	printf("2.)Cat\n");
-	printf("3.)Lion\n");
-	scanf_s("%d", &animalSelection);
-
-	switch (animalSelection)
+	if (myAnimal != NULL)
 	{
-	case ANIMAL:
-		printf("Selected base animal");
-		break;
-
-	case DOG:
-		myAnimal = malloc(sizeof(struct Dog));
-		//myAnimal = dogPtr;
 		printf("Selected dog\n");
 		printf("Enter dog's weight: ");
 		scanf_s("%lf", &weight);
 		printf("Enter dog's age:");
 		scanf_s("%d", &age);
-		
+
 		Construct_Dog(&myAnimal);
 		((struct Dog*)myAnimal)->weight = weight;
 		((struct Dog*)myAnimal)->age = age;
-		break;
-
-	case CAT:
-		catPtr = malloc(sizeof(struct Cat));
-		myAnimal = &catPtr;
-		printf("Selected cat");
-		printf("Enter cat's number of lives:");
-		scanf_s("%d", &numLives);
-		printf("Enter cat's age: %d");
-		scanf_s("%d", &age);
-
-		Construct_Cat(&catPtr);
-		catPtr->numberOfLives = numLives;
-		catPtr->age = age;
-		break;
-
-	case LION:
-		lionPtr = malloc(sizeof(struct Lion));
-		myAnimal = &lionPtr;
-		printf("Selected lion");
-		scanf_s("Enter lion's weight: %lf", &weight);
-		scanf_s("Enter lion's number of lives: %d", &numLives);
-		scanf_s("Enter lion's age: %d", &age);
-		Construct_Lion(&lionPtr);
-		lionPtr->weight = weight;
-		lionPtr->numberOfLives = numLives;
-		lionPtr->age = age;
-		break; 
-
-	default:
-		break;
 	}
-
-	printf("\n");
-	printf("\nSpeak animal!");
-	((void(*)(struct Animal*))&myAnimal->v_table[0])(myAnimal);
-	double cost = 1.0;//((double(*)(struct Animal*))&myAnimal->v_table[1])(myAnimal);
-	printf("\nYour animal costs %.2lf per month.\n",cost);
-
-	free(myAnimal);
-	return 1;
 }
